21f1376bf2fd09d282c51cc9dd01404435b692d3
Pili <pilarriusm@gmail.com>
2026-09-19 15:36:59 +0200

    Adjusting home page meesaging

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.7tshkr8upb"
+    "revision": "0.gtg7evntik"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
